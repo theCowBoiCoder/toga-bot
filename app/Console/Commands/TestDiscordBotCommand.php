fab67f357877f06843b3155da296489bb09f8633
Hayden Sweet <haydensweet29@gmail.com>
2021-04-15 16:18:38 +0100

    toga bot fix

--- app/Console/Commands/TestDiscordBotCommand.php
+++ app/Console/Commands/TestDiscordBotCommand.php
@@ -2,31 +2,13 @@
 
 namespace App\Console\Commands;
 
-use App\Discord\JoinTeam;
-use App\Discord\Member as DiscordMember;
+
 use App\Football\FootballAPI;
-use App\Football\Predictor;
-use App\Models\AllowedCommand;
-use App\Models\GamePredictor;
-use App\Http\Services\Discord\Commands\CommandHandler;
-use App\Models\Members;
-use App\Models\Result;
-use Carbon\Carbon;
-use Discord\Http\Http;
-use Discord\Parts\Channel\Channel;
 use Discord\Parts\Channel\Message;
 use Discord\Parts\Embed\Embed;
-use Discord\Parts\Guild\AuditLog\Options;
-use Discord\Parts\Guild\Guild;
-use Discord\Parts\User\Member;
-use Discord\Parts\User\User;
-use Discord\WebSockets\Event;
-use Discord\WebSockets\Events\GuildIntegrationsUpdate;
 use Illuminate\Console\Command;
 use Discord\Discord;
-use Illuminate\Support\Arr;
-use Illuminate\Support\Str;
-use RestCord\DiscordClient;
+
 
 class TestDiscordBotCommand extends Command
 {
@@ -58,117 +40,152 @@ class TestDiscordBotCommand extends Command
      * Execute the console command.
      *
      * @return int
+     * @throws \Discord\Exceptions\IntentException
      */
     public function handle()
     {
+
 
         $discord = new Discord([
             'token' => env('DISCORD_TOKEN')
         ]);
 
-
-        $discord->on('ready', function (Discord $discord)  {
-            $discord->on('message', function (Message $message) use ($discord) {
-
-
-                if (CommandHandler::check($message->content)) {
-                    CommandHandler::fire($message);
-                }else{
-                    $message->channel->sendMessage("Command not found!");
-                }
-
-
-                if ($message->content === '--join') {
-
-                }
-
-                if ($message->content === '--games today') {
-                    $today_games = Result::query()->where('status', '!=', 'FINISHED')->whereDate('created_at', Carbon::today()->toDateString())->get();
-                    $embedded = new Embed($discord);
-                    $embedded->setTitle("Great please choose from below who you want to predict for.");
-                    if (count($today_games) >= 1) {
-                        foreach ($today_games as $key => $game) {
-                            $embedded->addField(['name' => "[{$key}] {$game->home_team_name} vs {$game->away_team_name}", 'value' => "Odds Coming Soon"]);
-                        }
-
-                        $embedded->setDescription("To predict all you need to do is --predict [0] 1-2 you musy follow this format or the prediction will not work");
-
-                    } else {
-                        $embedded->addField(['name' => 'No Games', 'value' => "Sorry there are no games to predict today try again tomorrow."]);
-                    }
-                    $embedded->setFooter('POWERED BY TOGA BOT');
-                    $message->channel->sendEmbed($embedded)->then(function (Message $message) {
-                        $message->react('804130997863317595');
-                    });
-                }
-
-                //Set Predictions
-                if (Str::contains($message->content, '--predict') !== false) {
-                    //Explode the message
-                    $explode_message = explode(' ', $message->content);
-                    $match_id = $explode_message[1];
-                    $score = $explode_message[2];
-
-
-                    $results = Result::query()->where('status', '!=', 'FINISHED')->whereDate('created_at', Carbon::today()->toDateString())->get();
-                    //Check if the prediction has been sent.
-                    $check_prediction = Predictor::checkMatchPrediction($results[$match_id]->id, (int)$message->user_id);
-                    if ($check_prediction == null) {
-                        //Set the Prediction
-                        Predictor::setMatchPrediction($results[$match_id]->id, (int)$message->user_id, $score);
-                        $message->channel->sendMessage("Thanks! Your Prediction for {$results[$match_id]->home_team_name} Vs {$results[$match_id]->away_team_name} has been set.");
-                    } else {
-                        //@TODO - Need to later see if we can get the KICK OFF times so we can change the predictions up to kick off.
-                        $message->channel->sendMessage("Sorry you cannot change your prediction once you have changed it.");
-                    }
-                }
-
-                //Get My Predictions
-                if ($message->content === '--my predictions' || $message->content === '--mypredictions') {
-                    //Get the Games that are not finished
-                    $today_games = Arr::flatten(Result::query()->select('id')->where('status', '!=', 'FINISHED')->whereDate('created_at', Carbon::today()->toDateString())->get()->toArray());
-
-                    //If I have some predictions then show them if not say NO!
-                    $my_predictions = Predictor::getMyMatchPredictions((int)$message->user_id, $today_games);
-                    if (count($my_predictions) >= 1) {
-                        $embedded = new Embed($discord);
-                        $embedded->setTitle("These are your predictions");
-                        foreach ($my_predictions as $my_prediction) {
-                            $embedded->addField(['name' => "{$my_prediction->match->home_team_name} Vs {$my_prediction->match->away_team_name}", 'value' => (string)($my_prediction->result)]);
-                        }
-
-                        $embedded->setFooter('POWERED BY TOGA BOT');
-                        $message->channel->sendEmbed($embedded)->then(function (Message $message) {
-                            $message->react('804130997863317595');
-                        });
-                    } else {
-                        $message->channel->sendMessage("Sorry {$message->author->username} you have not set any predictions yet.");
-                    }
-
-                }
-                if ($message->content === '--topoftheleague') {
-
-                    $team = FootballAPI::getLeagueStandings(env('FOOTBALL_PREMIER_LEAGUE_ID'))[0];
-                    $embedded = new Embed($discord);
-                    $embedded->setTitle((string)$team->team->name);
-                    $embedded->setThumbnail('https://cdn.freebiesupply.com/images/large/2x/manchester-city-logo-png-transparent.png');
-                    $embedded->addField(['name' => 'Played', 'value' => $team->playedGames]);
-                    $embedded->addFieldValues('Won', $team->won, true);
-                    $embedded->addFieldValues('Draw', $team->draw, true);
-                    $embedded->addFieldValues('Lost', $team->lost, true);
-                    $embedded->addFieldValues('Points', $team->points, true);
-                    $embedded->addFieldValues('Scored', $team->goalsFor, true);
-                    $embedded->addFieldValues('Conceded', $team->goalsAgainst, true);
-                    $embedded->addFieldValues('Difference', $team->goalDifference, true);
-                    $embedded->setTimestamp();
-                    $embedded->setFooter('POWERED BY TOGA BOT');
-                    $message->channel->sendEmbed($embedded)->then(function (Message $message) {
-                        $message->react('804130997863317595');
-                    });
-                }
-            });
+        $discord->on('ready', function () use ($discord) {
+           $discord->on('message',function (Message $message) use ($discord){
+               if ($message->content === '--topoftheleague') {
+                   $team = FootballAPI::getLeagueStandings(env('FOOTBALL_PREMIER_LEAGUE_ID'))[0];
+                   $embedded = new Embed($discord);
+                   $embedded->setTitle((string)$team->team->name);
+                   $embedded->setThumbnail('https://cdn.freebiesupply.com/images/large/2x/manchester-city-logo-png-transparent.png');
+                   $embedded->addField(['name' => 'Played', 'value' => $team->playedGames]);
+                   $embedded->addFieldValues('Won', $team->won, true);
+                   $embedded->addFieldValues('Draw', $team->draw, true);
+                   $embedded->addFieldValues('Lost', $team->lost, true);
+                   $embedded->addFieldValues('Points', $team->points, true);
+                   $embedded->addFieldValues('Scored', $team->goalsFor, true);
+                   $embedded->addFieldValues('Conceded', $team->goalsAgainst, true);
+                   $embedded->addFieldValues('Difference', $team->goalDifference, true);
+                   $embedded->setTimestamp();
+                   $embedded->setFooter('POWERED BY TOGA BOT');
+                   $message->channel->sendEmbed($embedded)->then(function (Message $message) {
+                       $message->react('804130997863317595');
+                   });
+               }
+           });
         });
 
         $discord->run();
+//
+//        $discord->on('ready', function (Discord $discord)  {
+//
+////                if ($discord->user->id == 703077578419732543) {
+////                    dump('hello');
+////                    $member = new Member($discord);
+////                    $member->moveMember(819300656602939422);
+////                }
+//            });
+//
+////            $discord->on('message', function (Message $message) use ($discord) {
+////
+////
+////                if (CommandHandler::check($message->content)) {
+////                    CommandHandler::fire($message);
+////                }else{
+////                    $message->channel->sendMessage("Command not found!");
+////                }
+////
+////
+////                if ($message->content === '--join') {
+////
+////                }
+////
+////                if ($message->content === '--games today') {
+////                    $today_games = Result::query()->where('status', '!=', 'FINISHED')->whereDate('created_at', Carbon::today()->toDateString())->get();
+////                    $embedded = new Embed($discord);
+////                    $embedded->setTitle("Great please choose from below who you want to predict for.");
+////                    if (count($today_games) >= 1) {
+////                        foreach ($today_games as $key => $game) {
+////                            $embedded->addField(['name' => "[{$key}] {$game->home_team_name} vs {$game->away_team_name}", 'value' => "Odds Coming Soon"]);
+////                        }
+////
+////                        $embedded->setDescription("To predict all you need to do is --predict [0] 1-2 you musy follow this format or the prediction will not work");
+////
+////                    } else {
+////                        $embedded->addField(['name' => 'No Games', 'value' => "Sorry there are no games to predict today try again tomorrow."]);
+////                    }
+////                    $embedded->setFooter('POWERED BY TOGA BOT');
+////                    $message->channel->sendEmbed($embedded)->then(function (Message $message) {
+////                        $message->react('804130997863317595');
+////                    });
+////                }
+////
+////                //Set Predictions
+////                if (Str::contains($message->content, '--predict') !== false) {
+////                    //Explode the message
+////                    $explode_message = explode(' ', $message->content);
+////                    $match_id = $explode_message[1];
+////                    $score = $explode_message[2];
+////
+////
+////                    $results = Result::query()->where('status', '!=', 'FINISHED')->whereDate('created_at', Carbon::today()->toDateString())->get();
+////                    //Check if the prediction has been sent.
+////                    $check_prediction = Predictor::checkMatchPrediction($results[$match_id]->id, (int)$message->user_id);
+////                    if ($check_prediction == null) {
+////                        //Set the Prediction
+////                        Predictor::setMatchPrediction($results[$match_id]->id, (int)$message->user_id, $score);
+////                        $message->channel->sendMessage("Thanks! Your Prediction for {$results[$match_id]->home_team_name} Vs {$results[$match_id]->away_team_name} has been set.");
+////                    } else {
+////                        //@TODO - Need to later see if we can get the KICK OFF times so we can change the predictions up to kick off.
+////                        $message->channel->sendMessage("Sorry you cannot change your prediction once you have changed it.");
+////                    }
+////                }
+////
+////                //Get My Predictions
+////                if ($message->content === '--my predictions' || $message->content === '--mypredictions') {
+////                    //Get the Games that are not finished
+////                    $today_games = Arr::flatten(Result::query()->select('id')->where('status', '!=', 'FINISHED')->whereDate('created_at', Carbon::today()->toDateString())->get()->toArray());
+////
+////                    //If I have some predictions then show them if not say NO!
+////                    $my_predictions = Predictor::getMyMatchPredictions((int)$message->user_id, $today_games);
+////                    if (count($my_predictions) >= 1) {
+////                        $embedded = new Embed($discord);
+////                        $embedded->setTitle("These are your predictions");
+////                        foreach ($my_predictions as $my_prediction) {
+////                            $embedded->addField(['name' => "{$my_prediction->match->home_team_name} Vs {$my_prediction->match->away_team_name}", 'value' => (string)($my_prediction->result)]);
+////                        }
+////
+////                        $embedded->setFooter('POWERED BY TOGA BOT');
+////                        $message->channel->sendEmbed($embedded)->then(function (Message $message) {
+////                            $message->react('804130997863317595');
+////                        });
+////                    } else {
+////                        $message->channel->sendMessage("Sorry {$message->author->username} you have not set any predictions yet.");
+////                    }
+////
+////                }
+////                if ($message->content === '--topoftheleague') {
+////
+////                    $team = FootballAPI::getLeagueStandings(env('FOOTBALL_PREMIER_LEAGUE_ID'))[0];
+////                    $embedded = new Embed($discord);
+////                    $embedded->setTitle((string)$team->team->name);
+////                    $embedded->setThumbnail('https://cdn.freebiesupply.com/images/large/2x/manchester-city-logo-png-transparent.png');
+////                    $embedded->addField(['name' => 'Played', 'value' => $team->playedGames]);
+////                    $embedded->addFieldValues('Won', $team->won, true);
+////                    $embedded->addFieldValues('Draw', $team->draw, true);
+////                    $embedded->addFieldValues('Lost', $team->lost, true);
+////                    $embedded->addFieldValues('Points', $team->points, true);
+////                    $embedded->addFieldValues('Scored', $team->goalsFor, true);
+////                    $embedded->addFieldValues('Conceded', $team->goalsAgainst, true);
+////                    $embedded->addFieldValues('Difference', $team->goalDifference, true);
+////                    $embedded->setTimestamp();
+////                    $embedded->setFooter('POWERED BY TOGA BOT');
+////                    $message->channel->sendEmbed($embedded)->then(function (Message $message) {
+////                        $message->react('804130997863317595');
+////                    });
+////                }
+////            });
+//        });
+//
+//        $discord->run();
     }
 }
